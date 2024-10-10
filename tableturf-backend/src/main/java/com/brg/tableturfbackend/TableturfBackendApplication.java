53ba624a6be02cc9e214e0405320cc45cac245e4
Théo Brégégère <theo.bregegere@etu.u-bordeaux.fr>
2024-10-10 16:43:43 +0200

    Initial commit

---
+++ tableturf-backend/src/main/java/com/brg/tableturfbackend/TableturfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.brg.tableturfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableturfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableturfBackendApplication.class, args);
+	}
+
+}
---
+++ tableturf-backend/src/test/java/com/brg/tableturfbackend/TableturfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brg.tableturfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableturfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
